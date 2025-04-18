52ca37c9f192be28fd98d82be38a5481c52682c6
Алексей <zexdesgin@gmail.com>
2025-04-18 15:27:41 +0300

    loginmodal

--- components/shared/buttons/CustomButton.tsx
+++ components/shared/buttons/CustomButton.tsx
@@ -2,18 +2,22 @@ interface CustomButtonProps {
     label: string;
     className?: string;
     onClick: () => void;
+    disabled?: boolean; // ← добавь это
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
     label,
     className,
-    onClick
+    onClick,
+    disabled
+    
 }) => {
     return (
         <div 
             onClick={onClick}
             className={`w-full py-2 bg-black hover:bg-dark text-white text-center rounded-xl transition cursor-pointer ${className}`}
         >
+            disabled={disabled}
             {label}
         </div>
     )
