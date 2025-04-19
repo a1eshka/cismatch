1980570b145823f08e923374d8deaeb501769e67
Алексей <zexdesgin@gmail.com>
2025-04-19 22:21:21 +0300

    steam_login-v3

--- app/auth/login/steam.ts
+++ app/auth/login/steam.ts
@@ -6,8 +6,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         // Redirect the user to Steam for authentication
         const steamAuthUrl = 'https://steamcommunity.com/openid/login'; // The Steam OpenID login URL
-        const redirectUri = `${process.env.BASE_URL}/steam-login-callback`; // Define your callback URL
-        console.log(`Redirecting to Steam: ${steamAuthUrl}?openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.return_to=${encodeURIComponent(redirectUri)}`);
+        const redirectUri = `${process.env.NEXT_PUBLIC_SITE_URL}/steam-login-callback`; // Define your callback URL
+
         // Create the URL for the Steam login redirect
         const steamLoginUrl = `${steamAuthUrl}?openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.return_to=${encodeURIComponent(redirectUri)}`;
 
