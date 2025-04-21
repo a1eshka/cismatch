206b22b759d66336ba1c5d387f5e009e46692d0d
Алексей <zexdesgin@gmail.com>
2025-04-21 10:05:01 +0300

    post-id-v21

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,7 +1,5 @@
-import apiService from "@/app/services/apiService";
-
 const PostsPageDetail = async ({ params }: { params: { id: string } }) => {
-  const post = await apiService.get(`/api/post/${params.id}/`);
-  return <div>{post.title}</div>;
+  return <div>Post ID: {params.id}</div>;
 };
+
 export default PostsPageDetail;
